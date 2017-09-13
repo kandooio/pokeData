242f0f52a71894a65f163e6c6ab860864deaaa0c
Reuben Kandiah <yes@kandoo.io>
2017-09-13 13:32:43 +0100

    Update genOneGetStats.r

--- genOneGetStats.r
+++ genOneGetStats.r
@@ -1,11 +1,11 @@
 # genOneGetStats.r
 # A script for pulling various data points for Generation I pokemon via Bulbapedia
 
-# Initialise tidyverse package
+# In the first instance we will need to create a dataframe of the Base Stats for Generation I pkmn
+# This can be done using the rvest package which I will call using the custom function "GetNode"
+
 library(tidyverse)
 
-# In the first instance we will need to create a dataframe of the Base Stats for Generation I pkmn
-# This can be done using the rvest package which I will call using the custom function "getNode"
 # Initialise getNode function
 getNode <- function(url, xPathValue) {
   library(rvest)
@@ -14,17 +14,13 @@ getNode <- function(url, xPathValue) {
 }
 
 # Get Base Stats from Bulbapedia (first table on specified url)
-base.stats <-
-  as.data.frame(html_table(
-    getNode(
-      'https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_base_stats_(Generation_I)',
-      '//table'
-    )[1],
-    fill = TRUE
-  ))
+base.stats <-as.data.frame(html_table(getNode('https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_base_stats_(Generation_I)','//table')[1],
+    fill = TRUE))
 
 # Create a seperate custom function to return specific tabular data as a list from each pokemon's entry on bulbapedia
-pokeTable <- function(pkmn, tableNo) {
+# The poketable function allows you to input a pokemon name, and the appropriate bulbapedia page is crawled for html data
+# 
+pokeTable <- function(pkmn, tableNo,row,col) {
   require(rvest)
   url <-
     paste('https://bulbapedia.bulbagarden.net/wiki/',
@@ -34,9 +30,26 @@ pokeTable <- function(pkmn, tableNo) {
   file <- read_html(url)
   tables <- html_nodes(file, "table")
   data <-
-    as.list(as.data.frame(html_table(tables[tableNo], fill = TRUE)))
-  return(data)
+   as.data.frame(html_table(tables[tableNo], fill = TRUE))
+  return <- data[row,col]
+  return(return)
 }
 
 # Replacing the initial loop function with lapply
-hatch_time <- lapply(baseStats$Pokémon, pokeTable, tableNo = 21)
+print("Running hatch_time lapply")
+hatch_time <- as.data.frame(unlist(lapply(baseStats$Pokémon, pokeTable, tableNo = 21,row=1,col=1)))
+print("Running height lapply")
+height <- as.data.frame(unlist(lapply(baseStats$Pokémon, pokeTable, tableNo = 22,row=1,col=2)))
+print("Running weight lapply")
+weight <- as.data.frame(unlist(lapply(baseStats$Pokémon, pokeTable, tableNo = 23,row=1,col=2)))
+print("Running type1 lapply")
+type1 <- as.data.frame(unlist(lapply(baseStats$Pokémon, pokeTable, tableNo = 10,row=1,col=1)))
+print("Running type2 lapply")
+type2 <- as.data.frame(unlist(lapply(baseStats$Pokémon, pokeTable, tableNo = 10,row=1,col=2)))
+
+# Add the stat collection to the baseStats dataframe
+baseStats$hatch_time <- hatch_time[,1]
+baseStats$height <- height[,1]
+baseStats$weight <- weight[,1]
+baseStats$type1 <- type1[,1]
+baseStats$type2 <- type2[,1]
