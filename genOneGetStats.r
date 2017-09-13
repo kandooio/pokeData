b8c70c41800dedfb80ae6d9b1b811927e1e6098e
Reuben Kandiah <yes@kandoo.io>
2017-09-13 10:44:56 +0100

    Added getNode function

--- genOneGetStats.r
+++ genOneGetStats.r
@@ -1,39 +1,42 @@
 # genOneGetStats.r
 # A script for pulling various data points for Generation I pokemon via Bulbapedia
 
-library(rvest)
+# Initialise tidyverse package
 library(tidyverse)
 
-# Get Base Stats from Bulbapedia
-url <- 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_base_stats_(Generation_I)'
-file<-read_html(url)
-tables<-html_nodes(file, "table")
-tables[2]
-baseStats <- as.data.frame(html_table(tables[1], fill = TRUE))
+# In the first instance we will need to create a dataframe of the Base Stats for Generation I pkmn
+# This can be done using the rvest package which I will call using the custom function "getNode"
+# Initialise getNode function
+getNode <- function(url, xPathValue) {
+  library(rvest)
+  x <- read_html(url) %>% html_nodes(xpath = xPathValue)
+  return(x)
+}
 
+# Get Base Stats from Bulbapedia (first table on specified url)
+base.stats <-
+  as.data.frame(html_table(
+    getNode(
+      'https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_base_stats_(Generation_I)',
+      '//table'
+    )[1],
+    fill = TRUE
+  ))
 
-# Loop through each pokemon to get table extraction
-# while(i <= pokeCount){
-#   url <- paste('https://bulbapedia.bulbagarden.net/wiki/',baseStats$Pokémon[i],'_(Pok%C3%A9mon)',sep='')
-#   file<-read_html(url)
-#   tables<-html_nodes(file, "table")
-#   baseStats$catch_rate[i] <- as.character(as.data.frame(html_table(tables[18], fill = TRUE)))
-#   baseStats$gender_ratio[i] <- as.character(as.data.frame(html_table(tables[17], fill = TRUE))[,1][2])
-#   baseStats$hatch_time[i] <- as.character(as.data.frame(html_table(tables[21], fill = TRUE)))
-#   baseStats$height[i] <- as.character(as.data.frame(html_table(tables[22], fill = TRUE))[,2][1])
-#   baseStats$weight[i] <- as.character(as.data.frame(html_table(tables[23], fill = TRUE))[,2][1])
-#   print(paste('Run ',i,' of ',pokeCount,' complete ',sep=''))
-#   i <- i + 1
-# }
-
-pokeTable <- function(pkmn,tableNo){
+# Create a seperate custom function to return specific tabular data as a list from each pokemon's entry on bulbapedia
+pokeTable <- function(pkmn, tableNo) {
   require(rvest)
-  url <- paste('https://bulbapedia.bulbagarden.net/wiki/',pkmn,'_(Pok%C3%A9mon)',sep='')
-  file<-read_html(url)
-  tables<-html_nodes(file, "table")
-  data <- as.list(as.data.frame(html_table(tables[tableNo], fill = TRUE)))
+  url <-
+    paste('https://bulbapedia.bulbagarden.net/wiki/',
+          pkmn,
+          '_(Pok%C3%A9mon)',
+          sep = '')
+  file <- read_html(url)
+  tables <- html_nodes(file, "table")
+  data <-
+    as.list(as.data.frame(html_table(tables[tableNo], fill = TRUE)))
   return(data)
 }
 
 # Replacing the initial loop function with lapply
-hatch_time <- lapply(baseStats$Pokémon,pokeTable,tableNo=21)
+hatch_time <- lapply(baseStats$Pokémon, pokeTable, tableNo = 21)
